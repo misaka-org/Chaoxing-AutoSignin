a60fc130ea0621af49e69b09f364ed6f4f2e52d6
Misaka <misaka@yangrucheng.top>
2025-03-11 15:44:29 +0800

    修改验证码接口

--- apps/server/src/functions/captcha.ts
+++ apps/server/src/functions/captcha.ts
@@ -2,18 +2,20 @@ import { request } from '../utils/request';
 interface CaptchaResponse {
     status: number;
     msg: string;
-    validate: string;
+    data: {
+        validate: string;
+    };
 }
 
 /**
  * 获取滑块验证码
  * 
- * 请注意此接口同一IP每分钟至多调用30次
+ * 请注意此接口有频率限制
  * @returns 
  */
 export const getValidate = async () => {
-    const result = await request('https://cx-api.yangrucheng.top/get_captcha', {});
-    const data: CaptchaResponse = JSON.parse(result.data);
-    const validate: string = data.validate;
+    const result = await request('https://cx.micono.eu.org/api/validate', {});
+    const res: CaptchaResponse = JSON.parse(result.data);
+    const validate: string = res.data.validate;
     return validate;
 }
